4ccac0982bd58ad200cb518190e9b38c93a7aa92
DmitryP0l <dmitrypoletaeu@gmail.com>
2021-11-08 11:11:21 +0300

    commit

---
+++ HW14-LikeCommentShare/Cell/TableViewCell.swift
@@ -0,0 +1,113 @@
+//
+//  TableViewCell.swift
+//  HW14-LikeCommentShare
+//
+//  Created by lion on 5.11.21.
+//
+
+import UIKit
+
+protocol TableViewCellDelegate: AnyObject {
+    func likeAction(photoName: String)
+    func dislikeAction(photoName: String)
+    func commentAction(photoName: String, labelText: String)
+    func shareAction(photoName: String)
+    func bookmarkAction(photoName: String)
+}
+
+
+final class CellModel {
+    let photoImage: UIImage?
+    let photoName: String
+    var isLiked: Bool
+    var toBookmarks: Bool
+    
+    
+    init (initImageString: String) {
+        photoName = initImageString
+        isLiked = false
+        toBookmarks = false
+        photoImage = UIImage(named: initImageString)
+    }
+}
+
+final class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellBackgroundView: UIView!
+    @IBOutlet weak var buttonBackgroundView: UIView!
+    @IBOutlet weak var imageViewCell: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!    
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    @IBOutlet weak var commentLabel: UILabel!
+
+    
+    static let identifier = "TableViewCell"
+    private var bookmark: Bool = false
+    private var liked: Bool = false
+    private var namePhoto: String = ""
+    
+    weak var delegate: TableViewCellDelegate?
+    
+    
+    func setupWith(model: CellModel) {
+        
+        namePhoto = model.photoName
+        liked = model.isLiked
+        bookmark = model.toBookmarks
+        imageViewCell.image = model.photoImage
+        
+        
+        let likeButtonImage = UIImage(systemName: liked ? "heart.fill" : "heart")
+        likeButton.setImage(likeButtonImage, for: .normal)
+    
+        let bookmarkButtonImage = UIImage(systemName: bookmark ? "bookmark.fill" : "bookmark")
+        bookmarkButton.setImage(bookmarkButtonImage, for: .normal)
+        
+        
+    }
+    
+     
+   
+ 
+}
+
+
+// MARK: - Buttons Actions
+extension TableViewCell {
+  
+    
+    @IBAction func likeButtonAction(_ sender: UIButton) {
+        if liked == true {
+            delegate?.dislikeAction(photoName: namePhoto)
+        } else {
+            delegate?.likeAction(photoName: namePhoto)
+        }
+    }
+    
+    @IBAction func commentButtonAction(_ sender: UIButton) {
+        delegate?.commentAction(photoName: namePhoto, labelText: commentLabel.text ?? "err")
+    }
+   
+    @IBAction func shareButtonAction(_ sender: UIButton) {
+        delegate?.shareAction(photoName: namePhoto)
+    }
+    
+    @IBAction func bookmarkButtonAction(_ sender: UIButton) {
+       // надо прикрутить сюда тогл
+        delegate?.bookmarkAction(photoName: namePhoto)
+    }
+    
+}
+
+
+
+
+extension TableViewCell: ViewControllerDelegate {
+    func insertTextLabel(labelText: String) {
+        commentLabel.text = labelText
+    }
+    
+    
+}
---
+++ HW14-LikeCommentShare/View Controller/ViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  HW14-LikeCommentShare
+//
+//  Created by lion on 4.11.21.
+//
+
+
+import UIKit
+
+
+
+protocol ViewControllerDelegate: AnyObject {
+    func insertTextLabel(labelText: String)
+    
+}
+
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    private let dataSourceArray: [CellModel] = [
+        CellModel(initImageString: "img_1"),
+        CellModel(initImageString: "img_2"),
+        CellModel(initImageString: "img_3"),
+        CellModel(initImageString: "img_4"),
+        CellModel(initImageString: "img_5"),
+        CellModel(initImageString: "img_6"),
+        CellModel(initImageString: "img_7"),
+        CellModel(initImageString: "img_8"),
+        CellModel(initImageString: "img_9"),
+        CellModel(initImageString: "img_10"),
+        CellModel(initImageString: "img_11"),
+        CellModel(initImageString: "img_12"),
+        CellModel(initImageString: "img_13"),
+        CellModel(initImageString: "img_14"),
+        CellModel(initImageString: "img_15")
+    ]
+    
+//    private var personNameComment: String = ""
+//    private var textComment: String = ""
+    
+    weak var delegatik: ViewControllerDelegate?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: TableViewCell.identifier)
+    }
+}
+
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSourceArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier, for: indexPath) as! TableViewCell
+        cell.setupWith(model: dataSourceArray[indexPath.row])
+        cell.delegate = self
+        return cell
+    }
+}
+
+
+ 
+extension ViewController: TableViewCellDelegate {
+    
+    func likeAction(photoName: String) {
+        dataSourceArray.first(where: {$0.photoName == photoName })?.isLiked = true
+        let object = dataSourceArray.first(where: {$0.photoName == photoName })
+        object?.isLiked = true
+        tableView.reloadData()
+    }
+    
+    func dislikeAction(photoName: String) {
+        dataSourceArray.first(where: {$0.photoName == photoName })?.isLiked = false
+        tableView.reloadData()
+    }
+    
+    func commentAction(photoName: String, labelText: String) {
+        showAlert()
+    }
+                                       
+    func shareAction(photoName: String) {
+    }
+    
+    func bookmarkAction(photoName: String) {
+    }
+}
+
+// MARK: - UIAlertController
+extension ViewController {
+  
+    @objc private func showAlert() {
+        let alert = UIAlertController(
+            title: "hello",
+            message: "how are you?",
+            preferredStyle: .alert
+        )
+        
+        //        add 2 textfields
+        alert.addTextField { field in
+            field.placeholder = "add your name"
+            field.returnKeyType = .next
+            field.keyboardType = .default
+        }
+        alert.addTextField { field in
+            field.placeholder = "add comment"
+            field.returnKeyType = .continue
+            field.keyboardType = .default
+        }
+        
+            //        add buttons
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in
+            
+            //        read textfield values
+            guard let fields = alert.textFields, fields.count == 2 else {
+                return
+            }
+            let nameFields = fields[0]
+            let commentFields = fields[1]
+            guard let name = nameFields.text, !name.isEmpty,
+                  let comment = commentFields.text, !comment.isEmpty else {
+                      print("fill in the fields")
+                      return
+                  }
+            
+            print( "\(name): \(comment)")
+            
+            
+            self.delegatik?.insertTextLabel(labelText: "\(name): \(comment)")
+            self.tableView.reloadData()
+        }))
+        present(alert, animated: true)
+    }
+    
+}
+
+
